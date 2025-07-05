4e879fe2f0953a3e4238e16000207846348f13b6
JackNiu <goodnc@163.com>
2025-07-05 16:00:29 +0800

    use link mock data.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,22 +10,28 @@ export default function DashboardLayout({
 }>) {
   const pathname = usePathname();
   console.log("pathname", pathname);
+  const linkData = [
+    {
+      title: "About",
+      href: "/dashboard/about",
+    },
+    {
+      title: "Settings",
+      href: "/dashboard/settings",
+    },
+  ];
   return (
     <div className="border-2 border-dashed p-4 w-1/2 mx-auto mt-10">
       <h2>Dashboard Layout</h2>
       <div className="flex gap-4">
-        <Link
-          className={pathname === "/dashboard/about" ? "text-purple-500" : ""}
-          href="/dashboard/about">
-          About
-        </Link>
-        <Link
-          className={
-            pathname === "/dashboard/settings" ? "text-purple-500" : ""
-          }
-          href="/dashboard/settings">
-          Settings
-        </Link>
+        {linkData.map((item) => (
+          <Link
+            className={pathname === item.href ? "text-purple-500" : ""}
+            key={item.title}
+            href={item.href}>
+            {item.title}
+          </Link>
+        ))}
       </div>
       {children}
     </div>
