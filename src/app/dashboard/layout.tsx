b68608891032346cf863b2d08a67ce82fd629d3c
JackNiu <goodnc@163.com>
2025-07-05 15:50:32 +0800

    Add usePathname sample.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,32 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import path from "path";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  console.log("pathname", pathname);
   return (
-    <div>
+    <div className="border-2 border-dashed p-4 w-1/2 mx-auto mt-10">
       <h2>Dashboard Layout</h2>
+      <div className="flex gap-4">
+        <Link
+          className={pathname === "/dashboard/about" ? "text-purple-500" : ""}
+          href="/dashboard/about">
+          About
+        </Link>
+        <Link
+          className={
+            pathname === "/dashboard/settings" ? "text-purple-500" : ""
+          }
+          href="/dashboard/settings">
+          Settings
+        </Link>
+      </div>
       {children}
     </div>
   );
