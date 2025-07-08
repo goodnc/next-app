f6837cfd719346a2f3ee8ba33df1de1f347a4a92
JackNiu <goodnc@163.com>
2025-07-08 11:40:11 +0800

    Add 404 page.

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <div>自定义的全局 Not Found</div>;
+}
---
+++ src/app/test/not-found.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+  return <div>自定义 Not Found</div>;
+}
